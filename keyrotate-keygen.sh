75739e4ecdffa907cdb0c588872b29102d23f120
arun12cool <41614057+arun12cool@users.noreply.github.com>
2020-05-31 15:56:35 +0530

    Update keyrotate-keygen.sh

--- keyrotate-keygen.sh
+++ keyrotate-keygen.sh
@@ -39,15 +39,15 @@ Push_Key_OPS_Works()
 
   for new1 in `cat /tmp/ldapusers.txt`
   do
-    iamusers=`ldapsearch -LLL -x -b "dc=freshsso,dc=com" -D "administrator@freshsso.com" -h xxxx -w 'xxxx' "(&(objectClass=user)(cn=$new1))" | grep "mail" | cut -d ' ' -f2 | cut -d '@' -f1 | tr -d '.'`
+    iamusers=`ldapsearch -LLL -x -b "dc=freshsso,dc=com" -D "xxxxx" -h xxxx -w 'xxxx' "(&(objectClass=user)(cn=$new1))" | grep "mail" | cut -d ' ' -f2 | cut -d '@' -f1 | tr -d '.'`
 
-    ldapsearch -LLL -x -b "dc=freshsso,dc=com" -D "administrator@freshsso.com" -h xxxx -w 'xxxxx' "(&(objectClass=user)(cn=$new1))" | grep memberOf | cut -d':' -f2 | cut -d',' -f1 | awk -F '=' {'print $2'} > /tmp/grp.txt
+    ldapsearch -LLL -x -b "dc=freshsso,dc=com" -D "xxxx" -h xxxx -w 'xxxxx' "(&(objectClass=user)(cn=$new1))" | grep memberOf | cut -d':' -f2 | cut -d',' -f1 | awk -F '=' {'print $2'} > /tmp/grp.txt
 
     #Fetching Group for an user
 
     for grp in `cat /tmp/grp.txt`
     do
-      ldapsearch -x -b "dc=freshsso,dc=com" -D "administrator@freshsso.com" -h xxxx -w 'xxxx' "(&(objectClass=group)(cn=$grp))" | grep description | cut -d ":" -f2 | tr -d ' ' >> /tmp/acc_id.sh
+      ldapsearch -x -b "dc=freshsso,dc=com" -D "xxxx" -h xxxx -w 'xxxx' "(&(objectClass=group)(cn=$grp))" | grep description | cut -d ":" -f2 | tr -d ' ' >> /tmp/acc_id.sh
 
       sort -u /tmp/acc_id.sh > /tmp/acc_id.txt
 
