d0a4a1b8eef2d4e650ec05fb43f1ec535af696d6
Arun Krishnakumar <akrishnakumar@FWS-CHE-LT-2503.freshworks-corp.com>
2020-12-28 16:39:40 +0530

    cim

--- cim2/dailyrsyncVDI.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -avz --no-perms --no-owner --no-group --rsync-path='/usr/bin/sudo /usr/bin/rsync'  rsync@keygen-server.freshpo.com:/home/ /home/ --update --existing
--- cim2/old-scripts-arun/scripts-existing/setting.py
+++
@@ -1,4 +0,0 @@
-mysql_host='127.0.0.1'
-mysql_db='cim'
-mysql_username='root'
-mysql_password='gd123'
